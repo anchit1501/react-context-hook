669b27ce5673baedb1e843f1530f981047c7c6a9
anchit.aggarwal2 <anchit.aggarwal2@mindtree.com>
2019-12-16 14:46:27 +0530

    Updating Context Data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './Navbar';
 import BookList from './BookList';
 import ThemeContextProvider from './context/ThemeContext';
+import ThemeToggle from './ThemeToggle';
 function App() {
   return (
     <div className="App">
      <ThemeContextProvider>
      <Navbar/>
      <BookList/>
+     <ThemeToggle/>
      </ThemeContextProvider>
     </div>
   );
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,22 +1,23 @@
-import React,{createContext, Component} from 'react';
+import React, { createContext, Component } from "react";
 
 export const ThemeContext = createContext();
 
-class ThemeContextProvider extends Component{
-    state={
-        isLightTheme:true,
-        light:{syntax:"#555", ui: "#ddd", bg:"#eee"},
-        dark:{syntax:"#ddd", ui:"#333",bg:"#555"}
-    }
-
-    render(){
-        return(
-            
-            <ThemeContext.Provider value={{...this.state}}>
-            {this.props.children}{/* refers to the childred wrapped in ThemeContext */}
-            </ThemeContext.Provider>
-            
-        );
-    }
+class ThemeContextProvider extends Component {
+  state = {
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" }
+  };
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  };
+  render() {
+    return (
+      <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme}}>{/* passing function and values */}
+        {this.props.children}
+        {/* refers to the childred wrapped in ThemeContext */}
+      </ThemeContext.Provider>
+    );
+  }
 }
 export default ThemeContextProvider;
