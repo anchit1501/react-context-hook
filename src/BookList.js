a969df9601d949a14c3fb1731324bc6f51ab32a5
anchit.aggarwal2 <anchit.aggarwal2@mindtree.com>
2019-12-16 14:12:39 +0530

    Basic UI

---
+++ src/BookList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div className="book-list">
+    
+       <ul>
+         <li>Way of kings</li>
+         <li>name of wind</li>
+         <li>the final empire</li>
+       </ul>
+     
+    </div>
+  );
+}
+
+export default BookList;
