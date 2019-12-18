8582a1fa7aa6d8d3ec3b5bcf7244b4c543d67d6f
anchit.aggarwal2 <anchit.aggarwal2@mindtree.com>
2019-12-18 17:39:04 +0530

    Consuming Context using hooks

--- src/BookList.js
+++ src/BookList.js
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./context/ThemeContext";
 
-class BookList extends Component {
-  
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
-        <ul>
-          <li style={{background:theme.ui}}>Way of kings</li>
-          <li style={{background:theme.ui}}>name of wind</li>
-          <li style={{background:theme.ui}}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Way of kings</li>
+        <li style={{ background: theme.ui }}>name of wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
